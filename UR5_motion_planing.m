37099d43af9716b3880aa75d86eb01c7baa830ca
郭庭瑋 <145442655+TFox0613@users.noreply.github.com>
2023-09-19 21:41:05 +0800

    Add files via upload

--- UR5_motion_planing.m
+++ UR5_motion_planing.m
@@ -25,21 +25,41 @@ T06 = robot_UR5.fkine([pi/2,pi/2,0,0,0,0]);
 %   robot_UR5.ikine(T06)
 theda = robot_UR5.ikine(T06);
 %% 軌跡規劃
-%%%%%%%%%%%%%%%%  T  = transl(" position") * rpy2tr( "eurly angle")
-% T0 初始位置座標  T1 末端點位置座標 計算初始、末端Transfer Function 可以計算每一個關節angle
-T0 = transl(-0.39,  0.3 , 0.3) * rpy2tr( 0 , 0 , 0); 
-T1 = transl(-0.39,  -0.3 , 0.3) * rpy2tr( 0 , 0 , 0); 
+%%%  T  = transl(" position") * rpy2tr( "eurly angle")
+%%% T0 初始位置座標  T1 末端點位置座標 計算初始、末端Transfer Function 可以計算每一個關節angle
+T0 = transl(-0.39,  0.3 , 0.3) * rpy2tr( pi , 0 , 0); 
+T1 = transl(-0.39,  -0.3 , 0.3) * rpy2tr( pi , 0, 0); 
 theda0 = robot_UR5.ikine(T0);
 theda1 = robot_UR5.ikine(T1);
-t  = 0 : 0.05 :2;
-[q,qd,qdd] = jtraj(theda0,theda1,t );
-robot_UR5.plot( q, 'trail','b-','movie','traj1.gif')
+t  = 0 : 0.1 :5;
 
+%% jtraj 根據joint space因此給initial\end 位置與姿態 不會是直線也不會固定末端點姿態 
+% [q,qd,qdd] = jtraj(theda0,theda1,t );
+% figure(1)
+% robot_UR5.plot( q, 'trail','b-','movie','traj1.gif')
+% robot_UR5.plot( q, 'trail','b-')
+%% ctraj 根據 笛卡兒空間座標 給initial\end 位置與姿態 會連成直線得到所有末端姿態都為固定(z軸向下)
+% Ts為每個sampling time的 T06 Transfer function
+% 帶入inv_kinametic 得到每個sampling_time的角度
+Ts = ctraj(T0,T1,length(t));
+qs = robot_UR5.ikine(Ts);
+
+figure(2)
+robot_UR5.plot( qs, 'trail','b-','movie','traj3.gif')
 %% test
-Preal=zeros(41,3);
-for j=1:41
-    T=robot_UR5.fkine(q(j,:));
-    Preal(j,:) = T.t;
+a=size(qs);
+Pjreal=zeros(a(1),3);
+Pcreal=zeros(a(1),3);
+
+for j=1:a(1)
+    % Tj=robot_UR5.fkine(q(j,:));
+    Tc=robot_UR5.fkine(qs(j,:));
+    % Pjreal(j,:) = Tj.t;
+    Pcreal(j,:) = Tc.t;
+
 end
+
+
+
 
 
